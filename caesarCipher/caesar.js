0d79db5e1041860e2cbc2af3990f2b665f0b175b
gusty <gustavodiorlando@gmail.com>
2024-02-16 15:35:41 -0300

    Finish alphabet shift function

--- caesarCipher/caesar.js
+++ caesarCipher/caesar.js
@@ -1,34 +1,31 @@
-'abcdefghijklmnopqrstuvwxyz' // 0
-
-// Right shift
-'bcdefghijklmnopqrstuvwxyza' // 1
-'cdefghijklmnopqrstuvwxyzab' // 2
-'defghijklmnopqrstuvwxyzabc' // 3
-
-// Left shift
-'zabcdefghijklmnopqrstuvwxy' // -1
-'yzabcdefghijklmnopqrstuvwx' // -2
-'xyzabcdefghijklmnopqrstuvw' // -3
-
 function alphabetShift(factor) {
     factor = factor % 26;
     let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let shift = alpha;
     if (factor < 0) {
         for (let i = 0; i > factor; i--) {
-            alpha = alpha.slice(25) + alpha.slice(0, 25);
+            shift = shift.slice(25) + shift.slice(0, 25);
         }
     }
     if (factor > 0) {
         for (let i = 0; i < factor; i++) {
-            alpha = alpha.slice(1, 25) + alpha.slice(0, 1);
+            shift = shift.slice(1, 26) + shift.slice(0, 1);
         }
     }
-    return alpha;
+    const convertTable = {};
+    for (let i = 65; i <= 90; i++) {
+        convertTable[String.fromCharCode(i)] = shift[i - 65];
+    }
+    return convertTable;
 }
 
 function caesarCipher(string, factor) {
+    string = string.toUpperCase();
+    const convert = alphabetShift(factor);
+    string.split('').forEach(letter => {
 
-
+    })
+    return string;
 }
 
 module.exports = caesarCipher;
