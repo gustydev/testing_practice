8d4cd13ed71d5854597c22e2981b5715e35cb7d2
gusty <gustavodiorlando@gmail.com>
2024-02-16 11:47:29 -0300

    Write basic tests for caesar cipher

---
+++ caesarCipher/caesar.test.js
@@ -0,0 +1,20 @@
+const caesar = require('./caesar');
+
+test('factor of 3 left', () => {
+    expect(caesar('The quick brown fox jumped over the lazy dog', -3)).toBe('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD');
+    expect(caesar('Test (with) punctuation!', -3)).toBe('QBPQ (TFQE) MRKZQRXQFLK!');
+})
+
+test('factor of zero', () => {
+    expect(caesar('The quick brown fox', 0)).toBe('The quick brown fox');
+})
+
+test('factor of 3 right', () => {
+    expect(caesar('The quick brown fox jumped over the lazy dog', 3)).toBe('WKH TXLFN EURZQ IRA MXPSHG RYHU WKH ODCB GRJ');
+    expect(caesar('Test (with) punctuation!', 3)).toBe('WHVW (ZLWK) SXQFWXDWLRQ!');
+})
+
+test('very high factors', () => {
+    expect(caesar('Test of very high factors', 133742069).toBe('MXLM HY OXKR ABZA YTVMHKL'))
+    expect(caesar('Test of very high factors', -123456789).toBe('MXLM HY OXKR ABZA YTVMHKL'))
+})
